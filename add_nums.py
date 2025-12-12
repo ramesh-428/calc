c8a558d56070ca432578de1a055bffc60a6a2dc6
ramesh-428 <88284221+ramesh-428@users.noreply.github.com>
2025-12-12 15:37:26 +0530

    Update add_nums.py

--- add_nums.py
+++ add_nums.py
@@ -1,4 +1,9 @@
 def add_nums(a,b):
     return a+b
 
+def multiply_nums(a,b):
+    return a*b
+
 print(f'Addition of two nums:10 + 20 ={add_nums(10,20)}')
+
+print(f'Addition of two nums:10 * 20 ={multiply_nums(10,20)}')
